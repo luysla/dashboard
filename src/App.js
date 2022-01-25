8764919141c24590b25aef7697a1c3b6a0fcc086
luysla <dyana.tavares1@gmail.com>
2022-01-24 22:41:29 -0300

    Adding components in app (temporary)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './components/Header';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <></>
+    <>
+		  <Header />
+      <Menu />
+    </>
   )
 }
 
